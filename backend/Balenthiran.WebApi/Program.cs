97ff51d41eb2d46f98e39dfbad9b3dffca8b9de0
James Balenthiran <jamesmb26@hotmail.co.uk>
2026-02-14 12:24:58 +0000

    Open api generation

--- backend/Balenthiran.WebApi/Program.cs
+++ backend/Balenthiran.WebApi/Program.cs
@@ -8,15 +8,14 @@ builder.Services.AddBackendServices(builder.Configuration);
 
 // Standard API setup
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddOpenApi("openapi");
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
